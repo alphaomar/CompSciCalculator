c04cd87978ac5e1d64cb32c111b3e405e23806c3
omar <leighomar12654@gmail.com>
2025-02-14 15:55:40 +0000

    Created addition and subraction functions

---
+++ calculator.py
@@ -0,0 +1,15 @@
+def addition(num1 , num2):
+    result = num1 + num2
+    retun result
+
+
+
+def subraction(num1, num2):
+    if num1 > num2:
+
+        result= num1 - num2
+    else:
+        result= num2 - num1
+
+    return result
+        
